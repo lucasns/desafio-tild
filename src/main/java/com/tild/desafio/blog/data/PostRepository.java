db0492443da47b0163f32cfc4de5f09a06629423
Lucas Nunes <lucas.n15@outlook.com>
2017-10-21 04:11:02 -0200

    Add search methods

--- src/main/java/com/tild/desafio/blog/data/PostRepository.java
+++ src/main/java/com/tild/desafio/blog/data/PostRepository.java
@@ -1,7 +1,13 @@
 package com.tild.desafio.blog.data;
 
 import com.tild.desafio.blog.model.Post;
+import com.tild.desafio.blog.model.Tag;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByTags(Tag tag);
+    List<Post> findByTagsIn(List<Tag> tags);
 }
--- src/main/java/com/tild/desafio/blog/data/TagRepository.java
+++ src/main/java/com/tild/desafio/blog/data/TagRepository.java
@@ -1,8 +1,12 @@
 package com.tild.desafio.blog.data;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tild.desafio.blog.model.Tag;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    List<Tag> findByName(String name);
+    List<Tag> findByNameIn(List<String> names);
 }
