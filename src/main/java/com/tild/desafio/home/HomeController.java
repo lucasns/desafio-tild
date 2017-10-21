06d9e1e924879def350bbb8c65b4ccdd1fc5fb1c
Lucas Nunes <lucas.n15@outlook.com>
2017-10-21 04:41:34 -0200

    Add search by tags to HomeController

--- src/main/java/com/tild/desafio/home/HomeController.java
+++ src/main/java/com/tild/desafio/home/HomeController.java
@@ -1,28 +1,43 @@
 package com.tild.desafio.home;
 
 import com.tild.desafio.blog.data.PostRepository;
+import com.tild.desafio.blog.data.TagRepository;
 import com.tild.desafio.blog.data.UserRepository;
 import com.tild.desafio.blog.model.Post;
+import com.tild.desafio.blog.model.Tag;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     private PostRepository postRepository;
+    private TagRepository tagRepository;
 
     @Autowired
-    public HomeController(PostRepository postRepository, UserRepository userRepository) {
+    public HomeController(PostRepository postRepository, UserRepository userRepository, TagRepository tagRepository) {
         this.postRepository = postRepository;
+        this.tagRepository = tagRepository;
     }
 
     @GetMapping("/")
-    public ModelAndView index() {
+    public ModelAndView index(@RequestParam(name = "tags", required=false) List<String> tags) {
         ModelAndView mv = new ModelAndView("index");
+        if (tags != null) {
+            List<Tag> tagList = tagRepository.findByNameIn(tags);
+            if (tagList.size() > 0) {
+                mv.addObject("posts", postRepository.findByTagsIn(tagList));
+                return mv;
+            }
+        }
+        
         mv.addObject("posts", postRepository.findAll());
-
         return mv;
     }
 }
